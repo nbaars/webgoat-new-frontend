ec469a667d0586d3d7b7c88c690a016b620c7ad7
iluaboltov <iluaboltov@gmail.com>
2024-03-27 01:27:29 -0400

    Actions for auth

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,44 +1,47 @@
 "use client";
-import { randomUUID } from "crypto";
-import { cookies } from "next/headers";
-import axios from "axios";
-
-const auth =async (formData: string) => {
-  return await axios.post('http://localhost:8080/WebGoat/login', formData)
-}
-const userCreate = async (formData: string)=>{
-  return await axios.post('http://localhost:8080/WebGoat/register.mvc', formData)
-}
-
+import {LoginSchema, RegisterSchema} from "@/schemas";
+import {z} from "zod";
 
 export async function login(
-    prevState: string | undefined,
-    formData: FormData,
+    formData: z.infer<typeof LoginSchema>
 ) {
-  const username = formData.get("username");
-  const password = formData.get("password");
-  try {
-    await auth(`username=${username}&password=${password}`);
-  } catch (error) {
-    if (error) {
-      return 'Invalid login or password'
-    }
-    throw error;
+  const {username, password} = formData
+
+  const res = await fetch('http://localhost:8080/WebGoat/login', {
+    method: "POST",
+    headers: {
+      Accept: "application/form-field-data",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({username, password}),
+  });
+  if (res.status === 401) {
+    return { error: "Wrong username or password" };
+  } else if (res.status === 422) {
+    return { error: "Something went wrong" };
+  } else if (res.status === 200) {
+    return { success: "Logged in!" };
   }
-}export async function signup(
-    prevState: string | undefined,
-    formData: FormData,
+}
+export async function signup(
+    formData: z.infer<typeof RegisterSchema>
 ) {
-  const username: FormDataEntryValue | null = formData.get("username");
-  const password = formData.get("password");
-  const terms = formData.get("terms");
-  const confirmPassword = formData.get("confirmPassword");
-  try {
-    await userCreate(`username=${username}&password=${password}&matchingPassword=${confirmPassword}&agree=$agreed`);
-  } catch (error) {
-    if (error) {
-      return 'User already exists'
-    }
-    throw error;
+  const {username, password, matchingPassword, agree} = formData
+
+  const res = await fetch('http://localhost:8080/WebGoat/register.mvc', {
+    body: JSON.stringify({username, password, confirmPassword: matchingPassword, terms: agree}),
+    method: "POST",
+    headers: {
+      Accept: "application/form-field-data",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin" : "*",
+    },
+  })
+  if (res.status === 401) {
+    return { error: "User exist" };
+  } else if (res.status === 422) {
+    return { error: "Something went wrong" };
+  } else if (res.status === 200) {
+    return { success: "User created succesfully!" };
   }
 }
