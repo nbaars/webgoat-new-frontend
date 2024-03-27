2b4596e07f0099f27f92620421316482c60e5ace
iluaboltov <iluaboltov@gmail.com>
2024-03-27 01:27:00 -0400

    Deleted page about

--- src/app/about/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-type AboutLayoutProps = {
-  children: React.ReactNode;
-};
-export default function AboutLayout({ children }: Readonly<AboutLayoutProps>) {
-  return <>{children}</>;
-}
